863a9a8f1d7be055e3a8a772b0200ab2c9eb4594
syahm <syahmi9258@gmail.com>
2023-08-03 14:23:48 +0800

    part2 done

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:web_image_uploader/utils.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:web_image_uploader/resources/add_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -35,6 +42,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Uint8List? _image;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
 
   void selectImage() async {
     Uint8List img = await pickImage(ImageSource.gallery);
@@ -43,36 +52,74 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void saveProfile() async {
+    String name = nameController.text;
+    String bio = bioController.text;
+
+    // ignore: unused_local_variable
+    String resp =
+        await StoreData().saveData(name: name, bio: bio, file: _image!);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                _image != null
-                    ? CircleAvatar(
-                        radius: 128,
-                        backgroundImage: MemoryImage(_image!),
-                      )
-                    : const CircleAvatar(
-                        radius: 128,
-                        backgroundImage: NetworkImage(
-                            'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'),
-                      ),
-                Positioned(
-                  bottom: -10,
-                  left: 80,
-                  child: IconButton(
-                    onPressed: selectImage,
-                    icon: const Icon(Icons.add_a_photo),
-                  ),
-                )
-              ],
-            )
-          ],
+        child: Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.2,
+              vertical: MediaQuery.of(context).size.height * 0.1),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  const SizedBox(height: 50),
+                  _image != null
+                      ? CircleAvatar(
+                          radius: 128,
+                          backgroundImage: MemoryImage(_image!),
+                        )
+                      : const CircleAvatar(
+                          radius: 128,
+                          backgroundImage: NetworkImage(
+                              'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'),
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    right: 40,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 50),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Name',
+                  contentPadding: EdgeInsets.all(10),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: bioController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter bio',
+                  contentPadding: EdgeInsets.all(10),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: saveProfile,
+                child: const Text('Save Profile'),
+              )
+            ],
+          ),
         ),
       ),
     );
